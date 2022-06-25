77683187b26fd148f41d227fe087da3e2d5fb422
MarceloYevenesM <marcelo.yevenes.moreno@gmail.com>
2022-06-25 19:01:46 -0400

    added footer styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,89 @@
-import { Box, Flex } from '@chakra-ui/react'
-import React from 'react'
-
+import { Flex, Grid, GridItem, Image } from "@chakra-ui/react";
+import React from "react";
+import logoTw from "../assets/Logo-TW.png";
+import textTW from "../assets/talent-week.png";
+import logoCoder from "../assets/logo-coder.png";
+import SocialNetworks from "./SocialNetworks";
 
 const Footer = () => {
   return (
     <footer>
-        <Flex justify='space-around'>
-            <Box></Box>
-            <Box></Box>
-            <Box></Box>
-        </Flex>
-        <label>© Copyright 2021 - Todos los derechos reservados</label>
-    </footer>
-  )
-}
+      <Flex
+        w="100%"
+        h="320.4px"
+        bgColor="#131313"
+        justify="space-around"
+        alignItems="center"
+        wrap="wrap"
+      >
+        <div className="footer">
+          <Flex w="100%" justify="space-around" alignItems="center" wrap="wrap">
+            <Grid
+              w="233px"
+              h="224.2px"
+              templateRows="25% 1fr"
+              templateColumns="25% 1fr"
+              gap={1}
+            >
+              <GridItem rowSpan={1} colSpan={1}>
+                <Flex w="100%" h="100%" justify="center" alignItems="center">
+                  <Image w="48px" h="46px" src={logoTw} alt="Logo TW" />
+                </Flex>
+              </GridItem>
 
-export default Footer
+              <GridItem rowSpan={1} colSpan={1}>
+                <Flex w="100%" h="100%" justify="center" alignItems="center">
+                  <Image w="156px" h="26px" src={textTW} alt="Text TW" />
+                </Flex>
+              </GridItem>
+
+              <GridItem colStart={2} rowStart={2}>
+                <Grid w="100%" h="100%">
+                  <a href="#">Inscripcion</a>
+                  <a href="#">Agenda</a>
+                  <a href="#">Date a conocer</a>
+                  <a href="#">Empresas</a>
+                </Grid>
+              </GridItem>
+            </Grid>
+
+            <Grid
+              w="233px"
+              h="224.2px"
+              templateRows="25% 1fr"
+              templateColumns="1fr"
+              gap={1}
+            >
+              <GridItem rowSpan={1} colSpan={1}>
+                <Flex w="100%" h="100%" justify="center" alignItems="center">
+                  <Image
+                    w="227px"
+                    h="42px"
+                    src={logoCoder}
+                    alt="Logo CoderHouse"
+                    className="logoCoderHouse"
+                  />
+                </Flex>
+              </GridItem>
+
+              <GridItem colStart={1} rowStart={2}>
+                <Grid w="100%" h="100%" templateRows="1fr 1fr 1fr 1fr">
+                  <a href="#">Inscripcion</a>
+                  <a href="#">Agenda</a>
+                  <a href="#">Date a conocer</a>
+                </Grid>
+              </GridItem>
+            </Grid>
+
+            <SocialNetworks />
+          </Flex>
+        </div>
+        <div className="text">
+            <label>© Copyright 2021 - Todos los derechos reservados</label>
+        </div>
+      </Flex>
+    </footer>
+  );
+};
+
+export default Footer;
