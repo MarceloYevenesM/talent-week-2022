d83e8c0d33be446844839cf9d9ea36d814324519
sanmoyano <smoyano.di@gmail.com>
2022-06-26 18:33:47 -0300

    Initialize project using Create React App

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
-import Footer from "./components/Footer";
 
 const App = () => {
-  return (
-    <ChakraProvider>
-      
-      <div>
-        <Footer />
-      </div>
-    </ChakraProvider>
-  );
+    return (
+        <ChakraProvider>
+            <Stack>
+                
+            </Stack>
+        </ChakraProvider>
+    );
 };
 
 export default App;
