679b892b98ac393db4a5608a71a7a638e3d9ee5a
MarceloYevenesM <marcelo.yevenes.moreno@gmail.com>
2022-07-03 15:51:27 -0400

    feat: Business

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
-import Footer from "./components/Footer";
+import ContainerBusiness from "./components/ContainerBusiness";
 
 const App = () => {
   return (
     <ChakraProvider>
-      
       <div>
-        <Footer />
+        <ContainerBusiness />
       </div>
     </ChakraProvider>
   );
---
+++ src/components/ContainerBusiness.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "../styles/business.css";
+import logoIBM from "../assets/business/logo-ibm.png";
+import Business from "./Business";
+import { Box, Flex, Text } from "@chakra-ui/react";
+
+const ContainerBusiness = () => {
+  return (
+    <Box width="100%" height="30rem">
+      <Flex flexDirection="column" justify="space-evenly" width="100%" height="100%">
+        <Box>
+            <Text textTransform="uppercase" marginInlineStart="5rem" fontSize="3.75rem">
+            Empresas participantes que te están buscando
+            </Text>
+            <Text textTransform="uppercase" marginInlineStart="5rem" fontSize="3.75rem">
+            te están buscando
+            </Text>
+        </Box>
+        <Flex width="100%" gap="8rem" justify="space-evenly">
+          
+                <Business dark={false} logo={logoIBM} />
+                <Business dark={false} logo={logoIBM} />
+                <Business dark={false} logo={logoIBM} />
+                <Business dark={false} logo={logoIBM} />
+                <Business dark={false} logo={logoIBM} />
+                <Business dark={false} logo={logoIBM} />
+                <Business dark={false} logo={logoIBM} />
+                <Business dark={false} logo={logoIBM} />
+            
+          
+        </Flex>
+      </Flex>
+    </Box>
+  );
+};
+
+export default ContainerBusiness;
