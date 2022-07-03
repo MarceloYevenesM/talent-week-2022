51e512521dd5e86f716d28f9cdc594a75001fee3
Marcelo Yévenes <marcelo.yevenes.moreno@gmail.com>
2022-07-03 18:58:57 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from "react";
+import ReactDOM from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
-import Footer from "./components/Footer";
 
-const App = () => {
-  return (
-    <ChakraProvider>
-      
-      
-        <Footer />
-     
-    </ChakraProvider>
-  );
-};
+import theme from "./Theme/theme";
+import App from "./App";
 
-export default App;
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+    <React.StrictMode>
+        <ChakraProvider theme={theme}>
+            <App />
+        </ChakraProvider>
+    </React.StrictMode>,
+);
