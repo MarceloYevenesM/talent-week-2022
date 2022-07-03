d415d86989422b67fa03b0ffa246c6b9a2fe7bd5
sanmoyano <smoyano.di@gmail.com>
2022-07-03 20:30:07 -0300

    footer

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from "react";
+
 import EstosDias from "./Containers/EstosDias";
 import Portada from "./Containers/Portada";
+import Footer from "./Components/Footer";
 
 const App = () => {
     return (
         <>
             <Portada />
             <EstosDias />
+            <Footer />
         </>
     );
 };
 
-
-
-
+export default App;
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,89 +1,93 @@
 import React from "react";
-import { Flex, Grid, GridItem, Image } from "@chakra-ui/react";
-import SocialNetworks from "./SocialNetworks";
+import { Flex, Grid, GridItem, Image, Text } from "@chakra-ui/react";
+
 import logoTw from "../assets/Logo-TW.png";
 import textTW from "../assets/talent-week.png";
 import logoCoder from "../assets/logo-coder.png";
 
+import SocialNetworks from "./SocialNetworks";
+
 const Footer = () => {
-  return (
-    <footer>
-      <Flex
-        w="100%"
-        h={{ base: "700px", md: "400px", lg: "320px" }}
-        bgColor="#131313"
-        justify="space-around"
-        alignItems="center"
-        wrap="wrap"
-      >
-        <div className="footer">
-          <Flex w="100%" justify="space-around" alignItems="center" wrap="wrap">
-            <Grid
-              w="233px"
-              h="224.2px"
-              templateRows="25% 1fr"
-              templateColumns="25% 1fr"
-              gap={1}
+    return (
+        <footer>
+            <Flex
+                alignItems="center"
+                bgColor="#131313"
+                h={{ base: "700px", md: "400px", lg: "320px" }}
+                justify="space-around"
+                w="100%"
+                wrap="wrap"
             >
-              <GridItem rowSpan={1} colSpan={1}>
-                <Flex w="100%" h="100%" justify="center" alignItems="center">
-                  <Image w="48px" h="46px" src={logoTw} alt="Logo TW" />
-                </Flex>
-              </GridItem>
+                <div className="footer">
+                    <Flex alignItems="center" justify="space-around" w="100%" wrap="wrap">
+                        <Grid
+                            gap={1}
+                            h="224.2px"
+                            templateColumns="25% 1fr"
+                            templateRows="25% 1fr"
+                            w="233px"
+                        >
+                            <GridItem colSpan={1} rowSpan={1}>
+                                <Flex alignItems="center" h="100%" justify="center" w="100%">
+                                    <Image alt="Logo TW" h="46px" src={logoTw} w="48px" />
+                                </Flex>
+                            </GridItem>
 
-              <GridItem rowSpan={1} colSpan={1}>
-                <Flex w="100%" h="100%" justify="center" alignItems="center">
-                  <Image w="156px" h="26px" src={textTW} alt="Text TW" />
-                </Flex>
-              </GridItem>
+                            <GridItem colSpan={1} rowSpan={1}>
+                                <Flex alignItems="center" h="100%" justify="center" w="100%">
+                                    <Image alt="Text TW" h="26px" src={textTW} w="156px" />
+                                </Flex>
+                            </GridItem>
 
-              <GridItem colStart={2} rowStart={2}>
-                <Grid w="100%" h="100%">
-                  <a href="/#">Inscripcion</a>
-                  <a href="/#">Agenda</a>
-                  <a href="/#">Date a conocer</a>
-                  <a href="/#">Empresas</a>
-                </Grid>
-              </GridItem>
-            </Grid>
+                            <GridItem colStart={2} rowStart={2}>
+                                <Grid h="100%" w="100%">
+                                    <a href="/#">Inscripcion</a>
+                                    <a href="/#">Agenda</a>
+                                    <a href="/#">Date a conocer</a>
+                                    <a href="/#">Empresas</a>
+                                </Grid>
+                            </GridItem>
+                        </Grid>
 
-            <Grid
-              w="233px"
-              h="224.2px"
-              templateRows="25% 1fr"
-              templateColumns="1fr"
-              gap={1}
-            >
-              <GridItem rowSpan={1} colSpan={1}>
-                <Flex w="100%" h="100%" justify="center" alignItems="center">
-                  <Image
-                    w="227px"
-                    h="42px"
-                    src={logoCoder}
-                    alt="Logo CoderHouse"
-                    className="logoCoderHouse"
-                  />
-                </Flex>
-              </GridItem>
+                        <Grid
+                            gap={1}
+                            h="224.2px"
+                            templateColumns="1fr"
+                            templateRows="25% 1fr"
+                            w="233px"
+                        >
+                            <GridItem colSpan={1} rowSpan={1}>
+                                <Flex alignItems="center" h="100%" justify="center" w="100%">
+                                    <Image
+                                        alt="Logo CoderHouse"
+                                        className="logoCoderHouse"
+                                        h="42px"
+                                        src={logoCoder}
+                                        w="227px"
+                                    />
+                                </Flex>
+                            </GridItem>
 
-              <GridItem colStart={1} rowStart={2}>
-                <Grid w="100%" h="100%" templateRows="1fr 1fr 1fr 1fr">
-                  <a href="/#">Nosotros</a>
-                  <a href="/#">Comunidad</a>
-                  <a href="/#">Contacto</a>
-                </Grid>
-              </GridItem>
-            </Grid>
+                            <GridItem colStart={1} rowStart={2}>
+                                <Grid h="100%" templateRows="1fr 1fr 1fr 1fr" w="100%">
+                                    <a href="/#">
+                                        <Text textStyle={"p"}> Nosotros</Text>
+                                    </a>
+                                    <a href="/#">Comunidad</a>
+                                    <a href="/#">Contacto</a>
+                                </Grid>
+                            </GridItem>
+                        </Grid>
 
-            <SocialNetworks />
-          </Flex>
-        </div>
-        <div className="text">
-          <label>© Copyright 2021 - Todos los derechos reservados</label>
-        </div>
-      </Flex>
-    </footer>
-  );
+                        <SocialNetworks />
+                    </Flex>
+                </div>
+                <div className="text">
+                    <label>© Copyright 2021 - Todos los derechos reservados</label>
+                </div>
+            </Flex>
+        </footer>
+    );
 };
 
 export default Footer;
