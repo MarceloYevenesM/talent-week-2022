b66b69fe67da932e8555d83a102453df6844570c
sanmoyano <smoyano.di@gmail.com>
2022-07-03 21:05:45 -0300

    formulario

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from "react";
 
-
 import EstosDias from "./Containers/EstosDias";
 import Portada from "./Containers/Portada";
 import Footer from "./Components/Footer";
+import ContainerRegistration from "./Components/ContainerRegistration";
 
 const App = () => {
     return (
         <>
             <Portada />
             <EstosDias />
-             <ContainerRegistration />
+            <ContainerRegistration />
             <Footer />
         </>
     );
